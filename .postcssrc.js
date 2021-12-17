ae004b3abed8f21fb0d07a416f0bef866f7bad26
dxtripleh <bj.injeung@gmail.com>
2021-12-17 10:12:07 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,24 @@
+//ESM
+// import
+// export
+
+// import autoprefixer from 'autoprefixer'
+
+// export{
+//   plugins:[
+//     autoprefixer
+//   ]
+// }
+
+
+//CommonJS - node js 문법
+// require()
+// module.exports
+
+const autoprefixer = require('autoprefixer')
+
+module. exports ={
+  plugins:[
+    autoprefixer
+  ]
+}
